6b64d74c19e1ac71ec2de8d2ee8a270199577d54
Shashank Vimal <shashank.vimal@wdc.com>
2019-07-13 10:06:34 +0530

    version 0.3: Random number generator mersenne twister

--- RandomNumber/RandomNum.c
+++ RandomNumber/RandomNum.c
@@ -14,6 +14,13 @@
 #define R1				(M1%A1)
 #define R2				(M2%A2)
 
+/*Defines for mersenne twister*/
+#define N				(624)
+#define M				(397)
+#define A				(0x9908b0dfUL)
+#define U				(0x80000000UL)
+#define L				(U - 1)
+
 typedef struct
 {
 	int s1st;
@@ -22,6 +29,8 @@ typedef struct
 
 DP_LCG_SEED_t seeds = {1, 1};
 
+static unsigned int Arr[N];
+static int next;
 
 /* *********************************************************************************
 ** Function: Rng_randomNumber
@@ -184,6 +193,58 @@ int Rng_dualPhaseLcgRandomNum(void)
 	return result;
 	
 }
+static void Rng_seed4MersenneTwister(unsigned long seed)
+{
+	int i;
+	Arr[0] = (seed & 0xFFFFFFFFUL);
+	
+	for(i = 1; i < N; i++)
+	{
+		Arr[i] = (1812433253UL * (Arr[i - 1] ^ (Arr[i - 1] >> 30)) + i);
+		Arr[i] &= 0xFFFFFFFFUL;
+	}
+}
+/* *********************************************************************************
+** Function: Rng_mersenneTwister
+**
+** Description: This function generates random number with better randomness properties
+**          
+**
+** Input: None
+** 
+** Output: Random number 
+**
+** Remarks:
+************************************************************************************/
+unsigned long Rng_mersenneTwisterRandomNum()
+{
+	unsigned long y, a;
+	int i;
+	
+	if(next == N)
+	{
+		next = 0;
+		/*If Arr is consumed; regenerate Arr*/
+		for(i = 0; i < (N - 1); i++)
+		{
+			y = (Arr[i] & U) | (Arr[i] & L);
+			a = (y & 0x1UL) ? A : 0x0UL;
+			Arr[i] = Arr[(i + M) % N] ^ (y >> 1) ^ a;
+		}
+		y = (Arr[N - 1] & U) | (Arr[0] & L);
+		a = (y &0x1UL) ? A : 0x0UL;
+		Arr[N - 1] = Arr[M - 1] ^ ((y >> 1) ^ a);
+	}
+	y = Arr[next++];
+	
+	/*Improve Distribution*/
+	y ^= (y >> 11);
+	y ^= (y << 7) & 0x9d2c5680UL;
+	y ^= (y << 15) & 0xefc60000UL;
+	y ^= (y >> 18);
+	return y;
+}
+
 
 int main(int argc, char *argv[]) 
 {
@@ -193,7 +254,8 @@ int main(int argc, char *argv[])
 	PROFILE_INIT();
 	
 	PROFILE_BEGIN();
-	for(i = 0; i < 5; i++) printf("Random Number: %d\n", Rng_dualPhaseLcgRandomNum());
+	Rng_seed4MersenneTwister(234);
+	for(i = 0; i < 5; i++) printf("Random Number: %d\n", Rng_mersenneTwisterRandomNum());
 	PROFILE_END();
 	
 	PROFILE_SHOW_TIME_TAKEN();
