b3b8c2a5179242d19cf9451a3fb758bd05f3a1af
shashank <shashankvimal@gmail.com>
2020-03-18 21:51:57 +0530

    Version 0.0: Library clean up

--- Array/Array.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "mem_alloc.h"
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	return 0;
-}
